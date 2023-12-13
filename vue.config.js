bded93f734215bbcb59d1d3f7d58f4f55394ca2e
Tamirlan <tuarshintamir@gmail.com>
2023-12-13 12:40:54 +0500

    fix bugs and errors

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  pablicPath: `/SPA-on-Vue/`,
+  publicPath: `/SPA-on-Vue/`,
   transpileDependencies: true
 })
